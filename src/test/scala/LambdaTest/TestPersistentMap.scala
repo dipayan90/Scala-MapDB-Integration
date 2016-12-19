2c341a9c1d98ade515addefd686bbd27893e4fcc
dipayan90 <dipayan90@gmail.com>
2016-12-18 16:18:50 -0800

    Final submission

--- src/test/scala/LambdaTest/TestPersistentMap.scala
+++ src/test/scala/LambdaTest/TestPersistentMap.scala
@@ -2,13 +2,17 @@ package LambdaTest
 
 import com.fortysevendeg.lambdatest._
 import java.io.File
+import java.nio.file.{Files, Paths}
 
+import org.scalacheck.Arbitrary.arbitrary
 import com.persist.uw.examples.PersistentMap
+import org.scalacheck.Prop._
 import org.mapdb.DB.HashMapMaker
 
 import scala.collection.mutable
 import scala.collection.JavaConverters._
 import org.mapdb._
+import org.scalacheck.Gen
 
 // TODO Write PersistentMap.scala in
 // src/main/scala/com/persist/uw/examples/PersistentMap.scala
@@ -22,37 +26,61 @@ import org.mapdb._
 class TestPersistentMap extends LambdaTest {
   def act = {
     test("Persistent Map") {
-      assert(true,"NYI")
+      assert(true, "NYI")
     } +
-    test("persistent input using put method, and produces correct output using get method"){
-      val map = new PersistentMap[String,Int]
-      map.put("three",3)
-      map.put("four",4)
-      assertEq(3,map.get("three").getOrElse(0),"got first object from map")
-      assertEq(4,map.get("four").getOrElse(0),"got second object from map")
-    } +
-    test("persists input using put method, removes using remove method and produces correct output using get method"){
-      val map = new PersistentMap[String,Int]
-      map.put("three",3)
-      map.put("four",4)
-      map.put("five",5)
-      assertEq(4,map.get("four").getOrElse(0),"got object from the map, removing object now")
-      map.remove("four")
-      assert(!map.contains("four"),"key in hash-map doesn't exist anymore")
-    } +
-    test("persists input and then provides iterator to iterate over the entryset") {
-      val map = new PersistentMap[String,Int]
-      map.put("three",3)
-      map.put("four",4)
-      map.put("five",5)
-      val it = map.iterator
-      assertEq(3,it.size,"Iterator has exact number of elements as added")
+      test("File Open and Exists") {
+        val map = new PersistentMap[String, Int]
+        map.put("three", 3)
+        map.put("four", 4)
+        assert(Files.exists(Paths.get("file.db")))
+      } +
+      test("persistent input using put method, and produces correct output using get method") {
+        val map = new PersistentMap[String, Int]
+        map.put("three", 3)
+        map.put("four", 4)
+        assertEq(3, map.get("three").getOrElse(0), "got first object from map")
+        assertEq(4, map.get("four").getOrElse(0), "got second object from map")
+      } +
+      test("persists input using put method, removes using remove method and produces correct output using get method") {
+        val map = new PersistentMap[String, Int]
+        map.put("three", 3)
+        map.put("four", 4)
+        map.put("five", 5)
+        assertEq(4, map.get("four").getOrElse(0), "got object from the map, removing object now")
+        map.remove("four")
+        assert(!map.contains("four"), "key in hash-map doesn't exist anymore")
+      }
+  } +
+    test("ScalaCheck") {
+      test("persistent input using put method, and produces correct output using get method") {
+        assertSC() {
+          forAll {
+            (a: String, b: Int) =>
+              val map = new PersistentMap[String, Int]
+              val scalaHashMap = new mutable.HashMap[String, Int]
+              map.put(a, b)
+              scalaHashMap.put(a, b)
+              map.get(a) == scalaHashMap.get(a)
+          }
+        }
+      } +
+        test("persists input using put method, removes using remove method and produces correct output using get method") {
+          assertSC() {
+            forAll {
+              (a: String, b: Int) =>
+                val map = new PersistentMap[String, Int]
+                val scalaHashMap = new mutable.HashMap[String, Int]
+                map.put(a, b)
+                scalaHashMap.put(a, b)
+                map.remove(a) == scalaHashMap.remove(a)
+            }
+          }
+        }
     }
-  }
 }
 
 object TestPersistentMap {
-  def main(args:Array[String]):Unit = {
+  def main(args: Array[String]): Unit = {
     run("PersistentMap", new TestPersistentMap)
   }
 }
